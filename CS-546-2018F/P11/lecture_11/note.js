65de5a30b09d674d84f59a8d12640dd9f5a440c4
Master-Alcy <aijingxuan92@gmail.com>
2018-11-20 19:17:14 -0500

    L11 and L12 added

---
+++ CS-546-2018F/P11/lecture_11/note.js
@@ -0,0 +1,23 @@
+/**
+ * jQuery library is exported in $
+ * Basic CSS selectors:
+ * 
+ * <h2>MyName Is <span class=“my-name”>Phil</span></h2>
+ * <mainid=“my_bio”>This is a test <h2>I love blogging</h2></main>
+ * 
+ * Id: #my_bio
+ * <mainid=“my_bio”>This is a test <h2>I love blogging</h2></main>
+ * 
+ * Class: .my-name
+ * <span class=“my-name”>Phil</span>
+ * 
+ * Elements: h2
+ * <h2>MyName Is <span class=“my-name”>Phil</span></h2>
+ * <h2>I love blogging</h2>
+ * 
+ * Combinations: #my_bio h2
+ * <h2>I love blogging</h2>
+ */
+// 21511d257a741c42a051427260a4c004ae244338(Used for Sync)
+// Sync : 上传完成. GIST ID : 7156f1fc0a9987287a3a73a8aeba0c68 .
+// 请在其他计算机上复制并使用此ID以下载设置.
