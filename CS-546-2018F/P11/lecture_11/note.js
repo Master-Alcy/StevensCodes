3fe0580aebf3bd1ebf06f5b907c71e3f757fea23
Master-Alcy <aijingxuan92@gmail.com>
2018-11-20 19:36:36 -0500

    remove token

--- CS-546-2018F/P11/lecture_11/note.js
+++ CS-546-2018F/P11/lecture_11/note.js
@@ -18,6 +18,3 @@
  * Combinations: #my_bio h2
  * <h2>I love blogging</h2>
  */
-// 21511d257a741c42a051427260a4c004ae244338(Used for Sync)
-// Sync : 上传完成. GIST ID : 7156f1fc0a9987287a3a73a8aeba0c68 .
-// 请在其他计算机上复制并使用此ID以下载设置.
