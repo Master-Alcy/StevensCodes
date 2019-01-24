68e98d1d3135da08fad01ce6856c498ef925a283
Master-Alcy <aijingxuan92@gmail.com>
2019-01-24 15:12:32 -0500

    add

--- MyLearningSpot/OpenCourses/ucb61a/lab/lab00/lab00.py
+++
@@ -1,8 +0,0 @@
-def twenty_eighteen():
-    """Come up with the most creative expression that evaluates to 2018,
-    using only numbers and the +, *, and - operators.
-
-    >>> twenty_eighteen()
-    2018
-    """
-    return 2017+1
