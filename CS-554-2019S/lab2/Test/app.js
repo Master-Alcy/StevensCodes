90a00de2341bd1da3c870793854f36c915d7cd11
Master-Alcy <aijingxuan92@gmail.com>
2019-02-05 18:03:34 -0500

    add

--- CS-554-2019S/lab2/Test/app.js
+++ CS-554-2019S/lab2/Test/app.js
@@ -5,9 +5,10 @@ const static = express.static(__dirname + "/public");
 
 app.use(static); // Why not app.use("/public", static)
 
-app.get("/*", (req, res) => {
+app.use("/*", (req, res) => {
     res.status(404).json({error: `404: page not found`});
 });
+
 
 app.listen(3000, () => {
     console.log("Single page route at http://localhost:3000/");
