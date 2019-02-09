df8604683947a331d97c26f193304d7b0429d959
Master-Alcy <aijingxuan92@gmail.com>
2019-02-09 14:26:47 -0500

    lab2 v1.0

--- CS-554-2019S/lab1/final/README.md
+++
@@ -1,5 +0,0 @@
-# Proceedure
-
-1. npm install
-2. npm run seed
-3. npm start
--- CS-554-2019S/lab1/final/data/index.js
+++
@@ -1,6 +0,0 @@
-//This is operations with database
-const taskData = require("./taskData");
-
-module.exports = {
-    tasks: taskData
-};
