00a3bdcbe87eb9158f078a47ff598c5ef704b109
Master-Alcy <aijingxuan92@gmail.com>
2019-03-20 16:13:59 -0400

    add

--- CS-554-2019S/lab4/test/data/index.js
+++ CS-554-2019S/lab4/test/data/index.js
@@ -3,14 +3,17 @@ const data = require("./dummy.json");
 
 async function getById(id) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => { // find project
+        if ( id < 1 || id > 1000) {
+            throw "index out of range";
+        }
+        setTimeout(() => { // find people
             const currProj = data[id - 1];
             if (currProj) {
                 resolve(currProj);
             } else {
                 reject(new Error("Data not found"));
             }
-        }, 5000);
+        }, 5000); // 5s
     });
 }
 
--- CS-554-2019S/lab4/test/routes/redisRoute.js
+++ CS-554-2019S/lab4/test/routes/redisRoute.js
@@ -8,53 +8,52 @@ bluebird.promisifyAll(redis);
 const client = redis.createClient(); // localhost:6379
 
 const data = require('../data');
+const LRU = "myList";
+const HASH = "myHash"
 
 async function keepLast20(id) {
-    const queueLength = await client.llenAsync("last20"); // get length
+    const queueLength = await client.llenAsync(LRU); // get length
     if (queueLength === 20) { // should never be 21
-        await client.rpopAsync('last20'); // queue is 20, reduce to 19
+        await client.rpopAsync(LRU); // queue is 20, reduce to 19
     }
-    return await client.lpushAsync("last20", id); // add and becomes 20
+    return await client.lpushAsync(LRU, id); // add and becomes 20
 }
 
 router.get("/history", async (request, response) => {
     try {
         let resultArray = [];
-        const redisList = await client.lrangeAsync("last20", 0, -1); // get full list of keys
+        const redisList = await client.lrangeAsync(LRU, 0, -1); // get full list of keys
 
         for (let i = 0; i < redisList.length; i++) {
-            const entry = await client.getAsync(redisList[i]); // get data in redis
-            // resultArray.push(unflatten(JSON.parse(entry)));
+            const entry = await client.hgetAsync(HASH, redisList[i]); // get data in redis
             resultArray.push(JSON.parse(entry));
         }
-
         return response.status(200).json(resultArray);
     } catch (e) {
-        return response.status(500).json({ error: e });
+        return response.status(500).json({ error: "In get /history " + e });
     }
 });
 
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 router.get("/:id", async (request, response) => {
     const id = parseInt(request.params.id); // should be number
-    if (isNaN(id) || !isFinite(id)) {
+    if (!isNumeric(id)) {
         return response.status(400).json({ error: "id not valid" });
     }
-
     try {
-        const redisExist = await client.existsAsync(id); // check existence
+        const redisExist = await client.hexistsAsync(HASH, id); // check existence
 
         if (redisExist === 1) {
-            // console.log("In cache");
             await keepLast20(id); // renew queue
-            const redisData = await client.getAsync(id); // get from cache
-            // console.log(redisData);
+            const redisData = await client.hgetAsync(HASH, id); // get from cache
             return response.status(200).json(JSON.parse(redisData));
         } else { // id don't exist
-            // console.log("In disk");
             const resultDisk = await data.getById(id); // error goes to catch, slow read disk
             await keepLast20(id); // renew queue
-            // console.log(flatten(resultDisk));
-            await client.setAsync(id, JSON.stringify(resultDisk)); // save in redis
+            await client.hsetAsync(HASH, id, JSON.stringify(resultDisk)); // save in redis
             return response.status(200).json(resultDisk);
         }
     } catch (e) {
