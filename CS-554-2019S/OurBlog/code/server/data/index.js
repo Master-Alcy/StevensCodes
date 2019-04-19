3c22fb2e394e7bf91781431b736b4275cf4ad2bb
Master-Alcy <aijingxuan92@gmail.com>
2019-04-18 21:09:02 -0400

    add

--- CS-554-2019S/OurBlog/code/server/data/index.js
+++ CS-554-2019S/OurBlog/code/server/data/index.js
@@ -1,3 +1,22 @@
-const mongoData = require('./mongodb/mongoCollections');
+// const mongoData = require('./mongodb/mongoCollections');
 
-module.exports = mongoData;
+// module.exports = mongoData;
+
+const { prisma } = require('./prisma/hello-world/generated/prisma-client');
+
+async function main() {
+    const newLink = await prisma.createLink({
+        url: 'www.prisma.test',
+        description: 'Prisma Testing'
+    });
+    console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
+
+    const allLinks = await prisma.links();
+    console.log(allLinks);
+    console.log("======================");
+}
+
+console.log("+++++++++++++++++++++++++");
+main().catch(e => console.error(e));
+main().catch(e => console.error(e));
+main().catch(e => console.error(e));
